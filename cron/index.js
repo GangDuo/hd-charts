5385c27521e43cbae83bd99ee70238bb68b6022f
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-09-15 17:44:49 +0900

    最近14日間の客数を取得する

--- cron/index.js
+++ cron/index.js
@@ -1,10 +1,32 @@
 const mysql = require('promise-mysql');
+const moment = require('moment');
 
 const commandText = `
-  SELECT 1
+  SELECT DATE_FORMAT(\`date\`, '%Y-%m-%d') AS \`date\`,
+         DAYOFWEEK(\`date\`) AS 'day_of_week',
+         count(publicID) AS customer_traffic
+    FROM TJournals
+   WHERE (\`date\` BETWEEN ? AND ?)
+     AND (storeCD IN(SELECT code FROM stores WHERE isDM))
+GROUP BY DATE_FORMAT(\`date\`, '%Y-%m-%d'),
+         DAYOFWEEK(\`date\`)
 ;`;
 
+function term() {
+  const pattern = "YYYY-MM-DD"
+  const yesterday = () => moment().subtract(1, 'days')
+  let beginDate = moment().subtract(14, 'days')
+  let endDate = yesterday()
+
+  return {
+    beginDate: beginDate.format(pattern),
+    endDate: endDate.format(pattern),
+  }
+}
+
 (async function() {
+  const {beginDate, endDate} = term()
+
   const connection = await mysql.createConnection({
     host: process.env.HOST,
     port: process.env.PORT,
@@ -14,7 +36,7 @@ const commandText = `
   });
 
   try {
-    const rows = await connection.query(commandText);
+    const rows = await connection.query(commandText, [beginDate, endDate]);
     console.log(rows)
   } catch (e) {
     console.log(e);
