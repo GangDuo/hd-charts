ba960514b9e86a1c69f1926a0de5bee2acb14bb8
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-09-15 18:28:06 +0900

    データベース取得結果をCSVで保存する

--- cron/index.js
+++ cron/index.js
@@ -1,5 +1,10 @@
 const mysql = require('promise-mysql');
 const moment = require('moment');
+const fs = require("fs");
+const { AsyncParser } = require("json2csv");
+const { Readable } = require('stream');
+
+const ONE_WEEK = 7;
 
 const commandText = `
   SELECT DATE_FORMAT(\`date\`, '%Y-%m-%d') AS \`date\`,
@@ -37,10 +42,38 @@ function term() {
 
   try {
     const rows = await connection.query(commandText, [beginDate, endDate]);
+    const source = rows.reduce((ax, row, i)=>{
+      if(i < ONE_WEEK) {
+        ax.push({"前の期間": row['customer_traffic']})
+      } else {
+        ax[i - ONE_WEEK].x = row['date']
+        ax[i - ONE_WEEK]["現在の期間"] = row['customer_traffic']
+      }
+      return ax
+    }, [])
     console.log(rows)
+    console.log(source)
+    writeAsCsv({path: "../customer_traffic.csv",source: source})
   } catch (e) {
     console.log(e);
   } finally {
     await connection.end();
   }
 })()
+
+  async function writeAsCsv(options) {
+    options = options || {}
+    const dest = options.path ? fs.createWriteStream(options.path, 'utf8') : process.stdout
+    const opts = {
+      fields: ["x","現在の期間","前の期間"]
+    };
+    const transformOpts = { readableObjectMode: true, writableObjectMode: true };
+    const parser = new AsyncParser(opts, transformOpts);
+    const input = new Readable({ objectMode: true });
+    input._read = () => {}; // redundant? see update below
+    options.source.forEach(item => input.push(item));
+    input.push(null);
+    const processor = parser.fromInput(input).toOutput(dest);
+  
+    await processor.promise(false).catch(err => console.error(err));
+  }
